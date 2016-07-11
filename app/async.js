879a2d3969352fce396e73e5fbca11a71a2998ca
Deephan Mohan <deephan.mohan85@gmail.com>
2016-07-11 19:36:43 -0400

    commit: async.js
    committing solutions for async.js

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(
+      function(resolve, reject) {
+        resolve(value);
+      }
+    );
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(
+      function(resolve, reject){
+        var lst = ["Adam","Alex","Matt","Paul","Rebecca"];
+        /*fs.readFile(url,'utf8',function(err, data){
+          if(err) throw err;
+          var jsonData = JSON.parse(data);
+          for(var i=0; i<jsonData.length; i++)
+          lst.push(jsonData.name);
+        });
+        fs.close();*/
+        resolve(lst);
+      }
+    );
   }
 };
