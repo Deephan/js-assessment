2c781767350081a61f522f4a6f27c0a7dbd7c4a9
Deephan Mohan <deephan.mohan85@gmail.com>
2016-07-11 09:51:52 -0400

    commit

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    obj.fn = fn;
+    return obj.fn();
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
+    return constructor;
   },
 
   iterate: function(obj) {
+    var res = [];
+    for(var key in obj){
+      res.push("'"+key+": "+obj[key]+"'")
+    }
+    return res;
 
   }
 };
